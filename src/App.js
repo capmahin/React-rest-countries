91dbbeaef0172b6a9b751642d531f1f1b77b2ea2
capmahin <mahinulhuque1996@gmail.com>
2022-03-17 04:18:16 +0600

    add folder and New extension

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import Countries from './components/Countries/Countries';
 
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
+    <Countries></Countries>
       
     </div>
   );
 }
-function LoadCountries(){
+/* function LoadCountries(){
   const [countries, setCountries] = useState([]);
   useEffect( ()=>{
     fetch("https://restcountries.com/v3.1/all")
@@ -22,7 +23,7 @@ function LoadCountries(){
       <h1>Visiting Every country of the world !!!</h1>
       <h3>Available Countries: {countries.length}</h3>
       {
-        countries.map(country => <Country name={country.name.common}></Country> )
+        countries.map(country => <Country name={country.name.common}population={country.population}></Country> )
       }
     </div>
   )
@@ -31,8 +32,11 @@ function Country(props){
   return(
     <div>
       <h2>Name:{props.name}</h2>
+      <h2>Population:{props.population}</h2>
     </div>
   )
-}
+} */
+
+
 
 export default App;
