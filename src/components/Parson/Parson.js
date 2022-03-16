5723b65a8df40a643585857734707038259a0153
capmahin <mahinulhuque1996@gmail.com>
2022-03-17 04:29:52 +0600

    how to use css and how to import it in js

---
+++ src/components/Parson/Parson.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Parson = () => {
+    return (
+        <div>
+            <h1>This is parson component</h1>
+        </div>
+    );
+};
+
+export default Parson;
