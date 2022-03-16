7f670174c8a03dc420d21af3b29104955e03087b
capmahin <mahinulhuque1996@gmail.com>
2022-03-17 05:09:45 +0600

    add country name and population

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Country = () => {
+const Country = (props) => {
     return (
         <div>
-            
+            <h1>Country Name:{props.name}</h1>
+            <p>Population:{props.population}</p>
         </div>
     );
 };
