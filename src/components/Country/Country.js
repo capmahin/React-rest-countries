1717d470b68e09126f4de249cfd4f290479d2197
capmahin <mahinulhuque1996@gmail.com>
2022-03-17 07:01:11 +0600

    adding flags from flag

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -3,10 +3,11 @@ import './Country.css'
 
 const Country = (props) => {
     console.log(props.country)
-    const {area,region,population,name} = props.country;
+    const {area,region,population,name,flags} = props.country;
     return (
         <div className='country'>
             <h1>Country Name:{name.common}</h1>
+            <img src={flags.png} alt="" />
             <p>Population:{population}</p>
             <p><small>Area:{area}</small></p>
             <p><small>Region:{region}</small></p>
