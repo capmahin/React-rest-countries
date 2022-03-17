daa87542651a988fee4c480cbe7b4021e0835cdd
capmahin <mahinulhuque1996@gmail.com>
2022-03-17 06:21:27 +0600

    add new component country

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
+    console.log(props.country)
     return (
         <div className='country'>
-            <h1>Country Name:{props.name}</h1>
-            <p>Population:{props.population}</p>
-            <p><small>Area:{props.area}</small></p>
+            <h1>Country Name:{props.country.name.common}</h1>
+            <p>Population:{props.country.population}</p>
+            <p><small>Area:{props.country.area}</small></p>
+            <p><small>Region:{props.country.region}</small></p>
         </div>
     );
 };
