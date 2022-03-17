4a112d7c4ed2eb85100b8ba8764fdebe3b47f7cc
capmahin <mahinulhuque1996@gmail.com>
2022-03-17 06:29:57 +0600

    use distractaring

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -3,12 +3,13 @@ import './Country.css'
 
 const Country = (props) => {
     console.log(props.country)
+    const {area,region,population,name} = props.country;
     return (
         <div className='country'>
-            <h1>Country Name:{props.country.name.common}</h1>
-            <p>Population:{props.country.population}</p>
-            <p><small>Area:{props.country.area}</small></p>
-            <p><small>Region:{props.country.region}</small></p>
+            <h1>Country Name:{name.common}</h1>
+            <p>Population:{population}</p>
+            <p><small>Area:{area}</small></p>
+            <p><small>Region:{region}</small></p>
         </div>
     );
 };
